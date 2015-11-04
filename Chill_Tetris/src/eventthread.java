45b2d6f5f777a330da6577ba7beb03c93d661c55
nugisquare <nugisquare@gmail.com>
2015-11-05 00:53:28 +0900

    KeyEvent base

--- Chill_Tetris/src/eventthread.java
+++ Chill_Tetris/src/eventthread.java
@@ -1,7 +1,40 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class eventthread extends Thread {
+public class eventthread extends Thread implements KeyListener {
+	
+	static int x; static int y;
 
 	public void run() {
+		preset();
+	}
+	
+	public void preset() {
+		x = Core.x; y =  Core.y;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int keycode = e.getKeyCode();
+		switch(keycode) {
+			case KeyEvent.VK_LEFT:
+				x--;
+			case KeyEvent.VK_RIGHT:
+				x++;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
